1f0534525bb915c679b5355c9340930b304f97a7
GeorgeGPach <134173221+GeorgeGPach@users.noreply.github.com>
2023-05-20 19:49:24 -0500

    probando ejercicio 1

--- js/index.js
+++ js/index.js
@@ -1,8 +1,13 @@
-var a = new Date();
-dia=a.getDay();
-switch (dia) {
-  case 5:  alert("Viernes social");  break;
-  case 6:  alert("Sábado deportivo"); break;
-  case 0:  alert("Domingo familiar");   break;
-  default:  alert("Día de Semana");
+let dia = prompt("Ingrese el numero de día");
+if(dia >= 0 && dia <7){
+    alert("Es correcto");
+    switch(dia){
+        case 5:  alert("Viernes social");  break;
+        case 6:  alert("Sábado deportivo"); break;
+        case 0:  alert("Domingo familiar");   break;
+        default:  alert("Día de Semana");     
+    }
+}else{
+    alert("Ingrese un número válido");
 }
+
