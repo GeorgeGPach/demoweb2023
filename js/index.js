21ce6d7e2c1318102654bb09db446a40a3d3e23d
GeorgeGPach <134173221+GeorgeGPach@users.noreply.github.com>
2023-05-20 20:02:09 -0500

    valindando datos de dias 0 a 6

--- js/index.js
+++ js/index.js
@@ -1,12 +1,7 @@
 let dia = prompt("Ingrese el numero de día");
 if(dia >= 0 && dia <7){
     alert("Es correcto");
-    switch(dia){
-        case 5:  alert("Viernes social");  break;
-        case 6:  alert("Sábado deportivo"); break;
-        case 0:  alert("Domingo familiar");   break;
-        default:  alert("Día de Semana");     
-    }
+
 }else{
     alert("Ingrese un número válido");
 }
