356700f59343af437607b82c39838984db495e6b
GeorgeGPach <134173221+GeorgeGPach@users.noreply.github.com>
2023-05-27 16:12:54 -0500

    alert

--- LAB01/js/app.js
+++ LAB01/js/app.js
@@ -2,7 +2,10 @@
 let nombrePersona = prompt("Por favor ingrese su nombre: ");
 
 //Ingresar apellido parterno
-let appellidoPaterno = prompt("Por favor ingrese su apellido paterno: ")
+let appellidoPaterno = prompt("Por favor ingrese su apellido paterno: ");
 
 //Ingesar apellido materno
-let appellidoMaterno = prompt("Por favor ingrese su apellido materno: ")
+let appellidoMaterno = prompt("Por favor ingrese su apellido materno: ");
+
+//Mostrar en cuadro emergente los datos ingresados
+alert("Hola mundo");
