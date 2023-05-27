6b7a4b166077d41b43223e3db94e542657f8b666
GeorgeGPach <134173221+GeorgeGPach@users.noreply.github.com>
2023-05-27 16:11:42 -0500

    let apellidos y nombres

--- LAB01/js/app.js
+++ LAB01/js/app.js
@@ -1,1 +1,8 @@
+//Ingresar nombres
 let nombrePersona = prompt("Por favor ingrese su nombre: ");
+
+//Ingresar apellido parterno
+let appellidoPaterno = prompt("Por favor ingrese su apellido paterno: ")
+
+//Ingesar apellido materno
+let appellidoMaterno = prompt("Por favor ingrese su apellido materno: ")
